9ac81a094bc825cd717b53767d1d417fd0d4f3ed
Ray H <rjhern37@neiu.edu>
2021-03-08 22:22:03 -0600

    Buttons working

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // alert('Oy Oy mate!!!');
+
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#resetButton');
+
+p1Button.addEventListener('click', function () {
+	alert('Player 1 clicked');
+});
+
+p2Button.addEventListener('click', function () {
+	alert('Player 2 clicked');
+});
+
+resetButton.addEventListener('click', function () {
+	alert('Reset complete');
+});
