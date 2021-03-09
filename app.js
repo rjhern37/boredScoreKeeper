e580cb4f19997a9a62fc6adaf941646ad202d14d
Ray H <rjhern37@neiu.edu>
2021-03-08 22:39:31 -0600

    basic functionality

--- app.js
+++ app.js
@@ -4,14 +4,35 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#resetButton');
 
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+
+let isGameOver = false;
+
 p1Button.addEventListener('click', function () {
-	alert('Player 1 clicked');
+	if (!isGameOver) {
+		p1Score += 1;
+		if (p1Score === winningScore) {
+			isGameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
 });
-
 p2Button.addEventListener('click', function () {
-	alert('Player 2 clicked');
+	if (!isGameOver) {
+		p2Score += 1;
+		if (p2Score === winningScore) {
+			isGameOver = true;
+		}
+		p2Display.textContent = p2Score;
+	}
 });
 
-resetButton.addEventListener('click', function () {
-	alert('Reset complete');
-});
+// resetButton.addEventListener('click', function () {
+// 	p1Display.textContent = 0;
+// 	p2Display.textContent = 0;
+// });
